d807a1d243b8c255a4dde31763a787b6fa33b2e3
Brayan <brayansteedcastillomartinez123@gmail.com>
2023-09-20 13:54:33 -0500

    aplicacion del post

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hola mundo');
+    res.send('peticion get');
+});
+
+app.post('/', (req, res) => {
+    res.send('peticion post');
 });
 
 app.listen(3000, ()=> {
