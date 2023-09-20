a0e7dbd1d55b2a490c3017f38ac4d613699befa3
Brayan <brayansteedcastillomartinez123@gmail.com>
2023-09-20 16:10:36 -0500

    conexion a la base de datos

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const mysql2 = require ('mysql2');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+// ===  http ===
 
 app.get('/:name/:id', (req, res) => {
     const { name, id } = req.params;
@@ -24,6 +26,19 @@ app.put('/', (req, res) => {
 app.delete('/', (req, res) => {
     res.send('peticion delete');
 });
+
+// === db ==
+const connection = mysql2.createConnection({
+   host: 'localhost',
+   user: 'root',
+   password: 'toor',
+   database: 'tutorial',
+});
+
+connection.connect((err) => {
+    if(err) throw err;
+    console.log('BD conectada');
+})
 
 app.listen(3000, ()=> {
     console.log('servidor encendido');
