4e9ccc96ce24165f809f503101c96c4dc7cbd43d
Brayan <brayansteedcastillomartinez123@gmail.com>
2023-09-27 14:04:29 -0500

    REGISTRANDO RUTAS AL SERVIDOR

--- BACKEND/src/config/app.js
+++ BACKEND/src/config/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
+const userRoutes = require('../routes/user.routes');
+
 const app = express();
 
 // middleawares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+// endpoints
+app.use('/user', userRoutes);
 
 module.exports = app;
